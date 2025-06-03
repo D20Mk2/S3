1766155f8ea4620abc32cc59a6611ff17aa8a474
D20Mk2 <142171594+D20Mk2@users.noreply.github.com>
2025-06-03 17:21:39 +0300

    Первый пошёл
    Основа сборки

---
+++ kubejs/server_scripts/recipes.js
@@ -0,0 +1,157 @@
+ServerEvents.recipes(e => {
+    e.shapeless('2x minecraft:diamond', [
+        'minecraft:diamond',
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:lapis_lazuli', [
+        'minecraft:lapis_lazuli', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:emerald', [
+        'minecraft:emerald', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:raw_gold', [
+        'minecraft:raw_gold', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:redstone', [
+        'minecraft:redstone', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:quartz', [
+        'minecraft:quartz', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:coal', [
+        'minecraft:coal', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:raw_copper', [
+        'minecraft:raw_copper', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:raw_iron', [
+        'minecraft:raw_iron', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:bone_meal', [
+        'minecraft:bone_meal', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:clay_ball', [
+        'minecraft:clay_ball', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:slime_ball', [
+        'minecraft:slime_ball', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:prismarine_shard', [
+        'minecraft:prismarine_shard', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:prismarine_crystals', [
+        'minecraft:prismarine_crystals', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:gunpowder', [
+        'minecraft:gunpowder', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:echo_shard', [
+        'minecraft:echo_shard', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:blaze_powder', [
+        'minecraft:blaze_powder', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:glowstone_dust', [
+        'minecraft:glowstone_dust', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:ghast_tear', [
+        'minecraft:ghast_tear', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:feather', [
+        'minecraft:feather', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:brown_mushroom', [
+        'minecraft:brown_mushroom', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:red_mushroom', [
+        'minecraft:red_mushroom', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:crimson_fungus', [
+        'minecraft:crimson_fungus', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x minecraft:warped_fungus', [
+        'minecraft:warped_fungus', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x mekanism:fluorite_gem', [
+        'mekanism:fluorite_gem', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x mekanism:raw_osmium', [
+        'mekanism:raw_osmium', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x mekanism:raw_tin', [
+        'mekanism:raw_tin', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x mekanism:raw_lead', [
+        'mekanism:raw_lead', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x mekanism:raw_uranium', [
+        'mekanism:raw_uranium', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('2x create:raw_zinc', [
+        'create:raw_zinc', 
+        'minecraft:netherite_block'
+    ]).keepIngredient('minecraft:netherite_block')
+    e.shapeless('minecraft:blaze_rod', '4x minecraft:blaze_powder')
+}) //Рудоварка
+ServerEvents.recipes(e => {
+    e.shapeless('2x minecraft:cobbled_deepslate', [
+        'minecraft:iron_ingot',
+        'minecraft:cobbled_deepslate'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:cobblestone', [
+        'minecraft:iron_ingot',
+        'minecraft:cobblestone'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:tuff', [
+        'minecraft:iron_ingot',
+        'minecraft:tuff'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:diorite', [
+        'minecraft:iron_ingot',
+        'minecraft:diorite'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:end_stone', [
+        'minecraft:iron_ingot',
+        'minecraft:end_stone'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:netherrack', [
+        'minecraft:iron_ingot',
+        'minecraft:netherrack'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:calcite', [
+        'minecraft:iron_ingot',
+        'minecraft:calcite'
+    ]).keepIngredient('minecraft:iron_ingot')
+    e.shapeless('2x minecraft:obsidian', [
+        'minecraft:iron_ingot',
+        'minecraft:obsidian'
+    ]).keepIngredient('minecraft:iron_ingot')
+}) //Камешки
